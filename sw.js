00c24b496dc8312c01ac1d1ef76daf91e1df0cc7
talhabuttg06-design <talhabuttg06@gmail.com>
2026-06-21 17:18:48 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'talha-car-game-v1';
+const FILES_TO_CACHE = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
